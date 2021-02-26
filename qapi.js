9c5f4490b019c37d865a3c409ddcfca061fa57be
Maxim Slipenko <36362599+Maks1mS@users.noreply.github.com>
2021-02-26 16:32:30 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,35 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6038f1af7defbd001eed52be",
+				"6038f1af7defbd001eed52c0",
+				"6038f1ba1a979b001ff32399"
+			]
+		},
+		"6038f1af7defbd001eed52be": {
+			"id": "6038f1af7defbd001eed52be",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6038f1af7defbd001eed52c0": {
+			"id": "6038f1af7defbd001eed52c0",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"6038f1ba1a979b001ff32399": {
+			"id": "6038f1ba1a979b001ff32399",
+			"pageUrl": "2",
+			"name": "2"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
